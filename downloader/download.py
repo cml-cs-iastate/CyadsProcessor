9241018ada0effd2447fe8e03e30b398e0a977f6
Alex Gurganus <gurgalex@iastate.edu>
2019-12-26 03:52:01 -0600

    Probably no need to async download videos

--- downloader/download.py
+++ downloader/download.py
@@ -1,15 +1,7 @@
-from .tasks import download_videos_from_db
-
-
 class DownloadProcessor:
 
     def get(self, request):
-        print(request)
-        download_videos_from_db.delay()
         return 200
 
     def post(self, request):
-        download_videos_from_db.delay()
         return 200
-
-
