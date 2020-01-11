539a3a1e1887fadaefae5800b3c68846b8b5a56d
Alex Gurganus <gurgalex@iastate.edu>
2020-01-11 00:40:07 -0600

    Remove celery and redis deps

--- CyadsProcessor/celery.py
+++
@@ -1,8 +0,0 @@
-import os
-from celery import Celery
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'CyadsProcessor.settings')
-
-celery_app = Celery('downloader', redis_socket_connect_timeout=2, redis_socket_timeout=2)
-celery_app.config_from_object('django.conf:settings', namespace='CELERY')
-celery_app.autodiscover_tasks()
