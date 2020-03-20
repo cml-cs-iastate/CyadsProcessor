80422c2e47c55054e0f0a45932e83eee828f87e1
Alex Gurganus <gurgalex@iastate.edu>
2020-03-20 16:10:08 -0500

    More delegation to videomdetadata saver

--- ad_extension_pull/views.py
+++ ad_extension_pull/views.py
@@ -17,13 +17,12 @@ def view_process_videos_collected_from_extension(request):
 def process_videos_collected_from_extension():
     unprocessed_extension_vids = ProcessorAddsViaExtension.objects.filter(processed=False)
 
-    unproccessed_extension_log: ProcessorAddsViaExtension
-    for unproccessed_extension_log in unprocessed_extension_vids:
-        vid_id = unproccessed_extension_log.ad_video_id
-
-        if not video.checked:
-            batch_processor = BatchProcessor()
-            batch_processor.save_video_metadata([vid_id], is_ad=True)
-        unproccessed_extension_log.processed = True
-        unproccessed_extension_log.save()
-        logger.info("proccessed extesion video", url=vid_id, db_vid_id=video.id)
+    unprocessed_extension_log: ProcessorAddsViaExtension
+    for unprocessed_extension_log in unprocessed_extension_vids:
+        vid_id = unprocessed_extension_log.ad_video_id
+        batch_processor = BatchProcessor()
+        batch_processor.save_video_metadata([vid_id], is_ad=True)
+        unprocessed_extension_log.processed = True
+        unprocessed_extension_log.save()
+        vid_db_id = Videos.objects.get(url=vid_id)
+        logger.info("processed extension video", url=vid_id, vid_db_id=vid_db_id)
