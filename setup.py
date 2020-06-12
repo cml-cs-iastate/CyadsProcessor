1aedf67b1148362a668cc2ef2ee7d3c708040b0b
Alex Gurganus <gurgalex@iastate.edu>
2020-06-12 13:37:04 -0500

    Package processor

---
+++ setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_namespace_packages
+
+setup(
+    name="CyadsProcessor",
+    version="1.32.0",
+    description="CyadsProcessor",
+    url="https://github.com/cml-cs-iastate/CyadsProcessor",
+    license="MIT",
+    classifiers=[
+
+        ],
+    python_requires=">=3.7",
+    install_requires=[
+        'django>=3.0',
+        "django-extensions==2.2.9",
+        "google-cloud-pubsub==1.5.0",
+        "mysqlclient==1.4.6",
+        "python-dateutil==2.8.1",
+        "google-api-python-client>=1.8.4",
+        "requests==2.23.0",
+        "youtube-dl==2020.6.6",
+        "beautifulsoup4==4.9.1",
+        "tenacity==6.2.0",
+        "lxml==4.5.1",
+        "html5lib==1.0.1",
+        "gunicorn==20.0.4",
+        "more-itertools==8.3.0",
+        "video_metadata @ git+https://github.com/cml-cs-iastate/video_metadata",
+        "structlog==20.1.0",
+        "pytest",
+        "docutils",
+    ],
+    packages=find_namespace_packages(),
+)
